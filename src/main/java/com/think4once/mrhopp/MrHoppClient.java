e74c2f259ab7d7c7dc47c2d30bf48a64eb1862b5
think4once <thomastwins42@outlook.com>
2025-04-07 13:30:27 +0100

    Initial commit

---
+++ src/main/java/com/think4once/mrhopp/MrHoppClient.java
@@ -0,0 +1,10 @@
+package com.think4once.mrhopp;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MrHoppClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/think4once/mrhopp/MrHoppDataGenerator.java
@@ -0,0 +1,11 @@
+package com.think4once.mrhopp;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MrHoppDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
